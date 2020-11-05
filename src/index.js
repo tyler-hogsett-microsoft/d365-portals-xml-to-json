d4a1fe54ee6b127baae9cdab3b5c04fdd03c2409
Tyler Hogsett <tyhogset@microsoft.com>
2020-11-05 09:28:23 -0800

    extended sample to run jsonata query

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+const jsonata = require("jsonata");
 const parser = require("xml2json");
 
 const xml = "<foo attr=\"value\"><bar>widget</bar></foo>";
 console.log(`input -> ${xml}`);
 
 // xml to json
-const json = parser.toJson(xml, {reversible: true});
-console.log("to json -> %s", json);
+const json = JSON.parse(parser.toJson(xml, {reversible: true}));
+console.log(`to json -> ${JSON.stringify(json)}`);
+
+// reversible json: {"foo":{"attr":"value","bar":{"$t":"widget"}}}
+// "readable" goal: {"foo":{"attr":"value","bar":"widget"}}
+const expression = jsonata('{"foo":{"attr":foo.attr,"bar":foo.bar."$t"}}');
+const result = expression.evaluate(json);
+console.log(`through jsonata -> ${JSON.stringify(result)}`);
+
+const reverseExpression = jsonata(`{"foo":{"attr":foo.attr,"bar":{"$t":foo.bar}}}`);
+const reverseResult = reverseExpression.evaluate(result);
+console.log(`run through reverse query -> ${JSON.stringify(reverseResult)}`);
 
 // json to xml
-const backToXml = parser.toXml(json);
+const backToXml = parser.toXml(reverseResult);
 console.log("back to xml -> %s", backToXml);
